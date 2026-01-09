5dce1b52a6cac9e0e8ee05a407412bddb8400607
Ping-Hung <ko.binghong@gmail.com>
2026-01-09 06:53:08 -0500

    set up basic project structure and a run script

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # Matching-Engine
 A simple matching engine that I aim to build while learning C++ development
+
+
+## Requirements
+- CMake ≥ Version 3.16
+- `Ubuntu-22.04` and `python 3`
+
+# Run Instruction
+simply run `./run.sh` in your terminal
+
---
+++ run.sh
@@ -0,0 +1,48 @@
+#! /usr/bin/python3
+import os
+import sys
+import re
+
+def usage(prog_name: str):
+    print("Usage 1. generate build system: f{prog_name}")
+    print("Usage 2. run tests: f{prog_name} test")
+    print("Usage 3. run clean test artifact: f{prog_name} clean")
+
+
+# depending on input argument (sys.argv[1]), either test, build, or cleanup (remove executable) 
+argc = len(sys.argv)
+if argc > 2:
+    usage(sys.argv[0])
+    sys.exit(1)
+
+if argc == 1:
+    print("generating build system with cmake in build/...")
+    ret = os.system("cmake -S . -B build")
+    sys.exit(ret)
+
+# match statements are NOT switch statements, it's more similar to Rust/Haskell type pattern matching.
+# it will try to match one of the cases specified with the *case* keyword, if match, execute the statements
+# specified under the case keyword
+
+match sys.argv[1]:
+    case "test":
+        print("building project...")
+        if os.system("cmake --build build") != 0:
+            sys.exit(1)
+
+        print("running tests...")
+        if os.system("ctest --test-dir build --output-on-failure") != 0:
+            sys.exit(1)
+
+        print("tests passed")
+
+    case "clean":
+        print("cleaning...")
+        print("done")
+
+    case _:
+        # wild card case (none of the above matches)
+        usage(sys.argv[0])
+        sys.exit(1)
+
+sys.exit(0)
